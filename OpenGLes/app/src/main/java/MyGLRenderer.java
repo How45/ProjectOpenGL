ba29893c3279ffa10e1d9002d882eab6a1224856
How45 <mletelllier02@gmail.com>
2019-11-09 15:58:02 +0000

    Update MyGLRenderer.java

--- OpenGLes/app/src/main/java/MyGLRenderer.java
+++ OpenGLes/app/src/main/java/MyGLRenderer.java
@@ -14,12 +14,24 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 unused) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
-        mSquare.draw();
+        Matrix.setLookAtM(viewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
+
+        mSquare.draw(vPMatrix);
 
     }
+    private final float[] vPMatrix = new float[16];
+    private final float[] projectionMatrix = new float[16];
+    private final float[] viewMatrix = new float[16];
 
+    @Override
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+
         mSquare = new Square();
 
     }
